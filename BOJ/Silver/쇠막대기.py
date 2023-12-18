3a722f47f981366405b1e283ceb986765aca502f
Yujeong Jeong <jjungyu1282@gmail.com>
2023-12-18 19:32:30 +0900

    BOJ: silver 쇠막대기

--- BOJ/Silver/쇠막대기.py
+++ BOJ/Silver/쇠막대기.py
@@ -9,12 +9,13 @@ stack = []
 count = 0
 
 for i in range(len(data)-1):
-    if data[i] == '(':
+    if data[i] == "(":
         stack.append('(')
     else:
         stack.pop()
-        if data[i-1] == '(':
+        if data[i-1] == "(":
             count += len(stack)
         else:
             count += 1
+
 print(count)
