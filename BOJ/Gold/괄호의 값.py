078805fc15aba604a59608e4365be52ed9cb851d
Yujeong Jeong <jjungyu1282@gmail.com>
2023-12-21 20:52:20 +0900

    BOJ: gold

---
+++ BOJ/Gold/괄호의 값.py
@@ -0,0 +1,44 @@
+# https://www.acmicpc.net/problem/2504
+
+import sys
+input = sys.stdin.readline
+
+bracket = input()
+
+stack = []
+result = 0
+temp = 1
+
+for i in range(len(bracket)):
+    now = bracket[i]
+    # print("stack: ", stack)
+    # print("now: ", now)
+    # print("temp: ", temp, "result: ", result)
+    # print("----------------------")
+
+    if now == '(':
+        temp *= 2
+        stack.append(now)
+    elif now == '[':
+        temp *= 3
+        stack.append(now)
+    elif now == ')':
+        if not stack or stack[-1] == '[':
+            result = 0
+            break
+        if bracket[i-1] == '(':
+            result += temp
+        temp //= 2
+        stack.pop()
+    elif now == ']':
+        if not stack or stack[-1] == '(':
+            result = 0
+            break
+        if bracket[i-1] == '[':
+            result += temp
+        temp //= 3
+        stack.pop()
+
+if stack:
+    result = 0
+print(result)
