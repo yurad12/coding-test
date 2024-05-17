681748091ead2120e8a8bdd76709100482afd8cc
Yujeong Jeong <jjungyu1282@gmail.com>
2024-05-17 14:24:35 +0900

    BOJ: 숨바꼭질3 with 0-1 bfs

---
+++ BOJ/Gold/숨바꼭질3.py
@@ -0,0 +1,28 @@
+# https://www.acmicpc.net/problem/13549
+
+import sys
+input = sys.stdin.readline
+from collections import deque
+
+n, k = map(int, input().split())
+
+visited = [0] * (200001)
+visited[n] = 1
+q = deque([(0,n)])
+answer = 100001
+while q:
+    sec, now = q.popleft()
+    if now == k:
+        answer = min(answer, sec)
+
+    if now*2 < 100001 and not visited[now*2]:
+        q.append((sec,now*2))
+        visited[now*2] = 1
+    if now-1 >= 0 and not visited[now-1]:
+        q.append((sec+1, now-1))
+        visited[now-1] = 1
+    if now+1 < 100001 and not visited[now+1]:
+        q.append((sec+1, now+1))
+        visited[now+1] = 1
+
+print(answer)
