3d53486ca51aacbb378ce4b315da7e1a0a48b784
Yujeong Jeong <jjungyu1282@gmail.com>
2024-05-30 17:55:01 +0900

    BOJ: IF문 좀 대신 써줘 with binary search

---
+++ BOJ/Silver/ IF문 좀 대신 써줘.py
@@ -0,0 +1,26 @@
+# https://www.acmicpc.net/problem/19637
+# 이진 탐색, 가장 먼저 입력된 칭호
+
+import sys
+input = sys.stdin.readline
+
+def solution(n, m, characters):
+    result = []
+
+    for power in characters:
+        start, end = 0, n-1
+        result = 0
+        while start <= end:
+            mid = (start + end) // 2
+            if int(titles[mid][1]) >= power:
+                result = mid
+                end = mid - 1
+            else:
+                start = mid + 1
+        print(titles[result][0])
+
+if __name__ == "__main__":
+    n, m = map(int, input().split())
+    titles = [list(map(str.strip, input().split())) for _ in range(n)]
+    characters = [int(input()) for _ in range(m)]
+    solution(n, m, characters)
