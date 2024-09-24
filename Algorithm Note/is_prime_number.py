b6ee3936b72b99986ded9b395e2f66eb3991d95d
Yujeong Jeong <jjungyu1282@gmail.com>
2024-09-24 19:01:52 +0900

    fix: correct indentation

--- Algorithm Note/is_prime_number.py
+++ Algorithm Note/is_prime_number.py
@@ -5,7 +5,7 @@ def is_prime_number(x):
     for i in range(2, int(math.sqrt(x))+1):
         if x%i==0:
             return False
-        return True
+    return True
     
 print(is_prime_number(4)) # False
 print(is_prime_number(7)) # True
