4330d3b9c96cc9f40c131158afedeb3bc2f52c7f
Yujeong Jeong <jjungyu1282@gmail.com>
2024-10-01 18:18:29 +0900

    BOJ: 가장 긴 증가하는 부분 수열2 with binary search

---
+++ BOJ/Gold/가장 긴 증가하는 부분 수열2.py
@@ -0,0 +1,45 @@
+# https://www.acmicpc.net/problem/12015
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+A = list(map(int, input().split()))
+
+# sol1: 직접 구현
+def binary_search(dp, num):
+    start, end = 0, len(dp)-1
+
+    while start <= end:
+        mid = (start + end) // 2
+        if dp[mid] < num:
+            start = mid + 1
+        else:
+            end = mid - 1
+    
+    return start
+
+dp = []
+for a in A:
+    idx = binary_search(dp, a)
+
+    if idx >= len(dp):
+        dp.append(a)
+    else:
+        dp[idx] = a
+
+print(len(dp))
+
+
+# sol2: bisect 라이브러리 사용
+from bisect import bisect_left
+
+dp = []
+for a in A:
+    idx = bisect_left(dp, a)
+
+    if idx >= len(dp):
+        dp.append(a)
+    else:
+        dp[idx] = a
+
+print(len(dp))
