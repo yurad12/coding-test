31bc6b12a354f4570ae34770cd2cb1f4ea699f55
Yujeong Jeong <jjungyu1282@gmail.com>
2024-12-03 21:53:56 +0900

    Python: BOJ 가장 긴 증가하는 부분 수열2

--- Python/BOJ/Gold/가장긴증가하는부분수열2.py
+++ Python/BOJ/Gold/가장긴증가하는부분수열2.py
@@ -1,3 +1,29 @@
 # https://www.acmicpc.net/problem/12015
+import sys
+input = sys.stdin.readline
 
+N = int(input())
+A = list(map(int, input().split()))
+
+def binary_search(dp, num):
+    start, end = 0, len(dp)-1
+
+    while start <= end:
+        mid = (start + end) // 2
+        if dp[mid] < num:
+            start = mid + 1
+        else:
+            end = mid - 1
     
+    return start
+
+dp = []
+for a in A:
+    idx = binary_search(dp, a)
+
+    if idx >= len(dp):
+        dp.append(a)
+    else:
+        dp[idx] = a
+
+print(len(dp))
